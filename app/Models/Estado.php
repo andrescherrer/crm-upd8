6078dd8ef00908b6d94e20b81521b36568f4b817
André C. Scherrer <andrescherrer@gmail.com>
2024-07-04 05:03:55 -0300

    feat: update estado model file

--- app/Models/Estado.php
+++ app/Models/Estado.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Estado extends Model
 {
@@ -13,4 +14,9 @@ class Estado extends Model
         'nome',
         'uf',
     ];
+
+    public function cidades(): HasMany
+    {
+        return $this->hasMany(Cidade::class);
+    }
 }
